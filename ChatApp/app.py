8ee2b78afb97649e4e0b1c27a1e29c4f9a87285a
sasakifuruta <rixiaozig@gmail.com>
2024-11-14 21:49:21 +0900

    テンプレートの追加”

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -2,23 +2,22 @@
 # アプリタイトル画面の新規登録ボタンを押すと、優母モーダル画面が表示される。その画面の「続ける」ボタン（エンドポイント'/next_step_s'とした）を押した際の処理を以下に実装。時間を取得し、条件分岐。
 @app.route('/next_step_s')
 def show_signup():
-  now = datetime.datetime.now()
-  now_hour = now.hour
-  if (22 <= now_hour < 24) or (0 <= now_hour < 6):
-    return render_template('registration/anger-mom.html')
-  else:
-    return render_template("新規登録html画面")
+    now = datetime.datetime.now()
+    now_hour = now.hour
+    if (22 <= now_hour < 24) or (0 <= now_hour < 6):
+        return render_template('anger-mom.html')
+    else:
+        return render_template("新規登録html画面")
 
-  
 
 # ログインページの表示
 # アプリタイトル画面のログインボタンを押すと、優母モーダル画面が表示される。その画面の「続ける」ボタン（エンドポイント'/next_step_l'とした）を押した際の処理を以下に実装。時間を取得し、条件分岐。
 
 @app.route('/next_step_l')
 def show_login():
-  now = datetime.datetime.now()
-  now_hour = now.hour
-  if (22 <= now_hour < 24) or (0 <= now_hour < 6):
-    return render_template('registration/anger-mom.html')
-  else:
-    return render_template("ログインhtml画面")
+    now = datetime.datetime.now()
+    now_hour = now.hour
+    if (22 <= now_hour < 24) or (0 <= now_hour < 6):
+        return render_template('anger-mom.html')
+    else:
+        return render_template("ログインhtml画面")
