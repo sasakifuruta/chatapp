1eb3bdabce02e662cfb074e6136209b7054c622f
s0s0g1 <passione0716@gmail.com>
2024-11-17 20:21:03 +0900

    チャットグループ編集、削除検証完了です

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -160,55 +160,56 @@ def add_chat_group():
     if chat_group == None:
         dbConnect.addGroup(uid, chat_group_name, group_img)
         return redirect('/')
-    else:
-        error = '既に同じ名前のチャットグループが存在しています'
-        return render_template('error/error.html', error_message=error)
+    # else:
+    #     error = '既に同じ名前のチャットグループが存在しています'
+    #     return render_template('error/error.html', error_message=error)
+
+@app.route('/edit_group')
+def edit_group():
+    # uid = session.get("uid")
+    uid = '970af84c-dd40-47ff-af23-282b72b7cca8'
+    # if uid is None:
+    #     return redirect('/process_login')
+    # return render_template('group.html', chat_groups=chat_groups, uid=uid)
+    return render_template('edit_group.html')
 
 
-# # チャットグループの更新
-# @app.route('/update_chat_group', methods=['POST'])
-# def update_chat_group():
-#     uid = session.get("uid")
-#     if uid is None:
-#         return redirect('/login')
+# チャットグループの更新
+@app.route('/update_chat_group', methods=['POST'])
+def update_chat_group():
+    # uid = session.get("uid")
+    uid = '970af84c-dd40-47ff-af23-282b72b7cca8'
+    # if uid is None:
+    #     return redirect('/login')
 
-#     cid = request.form.get('cid')
-#     chat_group_name = request.form.get('chat_groupTitle')
-#     chat_group_description = request.form.get('chat_groupDescription')
+    # cid = request.form.get('cid')
+    cid= 1
+    group_img= "noimg"
+    chat_group_name = request.form.get('chat_groupTitle')
 
-#     dbConnect.updateChat_group(uid, channel_name, channel_description, cid)
-#     return redirect('/detail/{cid}'.format(cid = cid))
-
-
-# # チャットグループの削除
-# @app.route('/delete/<cid>')
-# def delete_chat_group(cid):
-#     uid = session.get("uid")
-#     if uid is None:
-#         return redirect('/login')
-#     else:
-#         chat_group = dbConnect.getChat_groupById(cid)
-#         if chat_group["uid"] != uid:
-#             flash('チャットグループは作成者のみ削除可能です')
-#             return redirect ('/')
-#         else:
-#             dbConnect.deleteChat_group(cid)
-#             chat_groups = dbConnect.getChat_groupAll()
-#             return redirect('/')
+    dbConnect.updateGroup(uid, chat_group_name, group_img, cid)
+    return redirect('/')
 
 
-# # チャットグループ詳細ページの表示
-# @app.route('/detail/<cid>')
-# def detail(cid):
-#     uid = session.get("uid")
-#     if uid is None:
-#         return redirect('/login')
+# チャットグループの削除
+@app.route('/delete/<cid>')
+def delete_chat_group(cid):
+    # uid = session.get("uid")
+    uid = '970af84c-dd40-47ff-af23-282b72b7cca8'
+    cid= 1
+    # if uid is None:
+    #     return redirect('/login')
+    # else:
+    # chat_group = dbConnect.getGroupById(cid)
+        # if chat_group["uid"] != uid:
+        #     flash('チャットグループは作成者のみ削除可能です')
+        #     return redirect ('/')
+        # else:
+    dbConnect.deleteGroup(cid)
+    # chat_groups = dbConnect.getGroupAll()
+    return redirect('/')
 
-#     cid = cid
-#     chat_group = dbConnect.getChat_groupById(cid)
-#     messages = dbConnect.getMessageAll(cid)
 
-#     return render_template('detail.html', messages=messages, chat_group=chat_group, uid=uid)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
