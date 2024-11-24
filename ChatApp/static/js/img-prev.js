9be12610473e9847fdde5a5bec24358bcd8b8bd7
souyama77 <p14p083@yahoo.co.jp>
2024-11-24 09:50:25 +0900

    ヘッダーエラーありテンプレート作業

--- ChatApp/static/js/img-prev.js
+++ ChatApp/static/js/img-prev.js
@@ -1,18 +1,20 @@
 // プロフィール画像ファイル選択時のプレビュー
 // 画像をアップロードした時に発火
-document.getElementById('file_upload').addEventListener('change', (e) => {
-    defaultIcon = document.getElementById('default-icon');
-    jsPreview = document.getElementById("jsPreview");
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('file_upload').addEventListener('change', (e) => {
+        defaultIcon = document.getElementById('default-icon');
+        jsPreview = document.getElementById("jsPreview");
 
-    const file = e.target.files[0];
-    console.log(file)
-    if (file) {
-        const fileReader = new FileReader();
-        fileReader.onload = (event) => {
-            jsPreview.style.display = 'block';
-            jsPreview.src = event.target.result;
-            defaultIcon.style.display = 'none';
+        const file = e.target.files[0];
+        console.log(file)
+        if (file) {
+            const fileReader = new FileReader();
+            fileReader.onload = (event) => {
+                jsPreview.style.display = 'block';
+                jsPreview.src = event.target.result;
+                defaultIcon.style.display = 'none';
+            }
+            fileReader.readAsDataURL(file);
         }
-        fileReader.readAsDataURL(file);
-    }
+    })
 });
